a0693747d03f49a661bc9260780b23d4f6767398
aryansingh-123 <aksingh2558@gmail.com>
2023-07-25 00:41:07 +0530

    Added more DATA to the app

--- src/Data/QuizData.js
+++ src/Data/QuizData.js
@@ -20,8 +20,28 @@ export const QuizData = [
         answer:2
     },
     {
-        question: "Which language runs in a web browser?",
-        options:[ "Java", "C", "Python", "JavaScript"],
+        question: " Which programming language is primarily used for developing mobile applications for Android devices?",
+        options:[ "Swift", "Java", "C#","Kotlin"],
         answer: 4
+    },
+    {
+        question: "Which programming language is commonly used for web development and allows for server-side scripting?",
+        options:[ "JavaScript", "Ruby","HTML", "CSS"],
+        answer: 1
+    },
+    {
+        question: "Which programming language is a statically-typed language developed by Apple for iOS and macOS app development?",
+        options:[ "Objective-C" ,"Java", "Swift" ,"C++"],
+        answer: 3
+    },
+    {
+        question: "Which programming language is often used for data analysis, scientific computing, and machine learning?",
+        options:[ "Java", "C++","R"," Perl"],
+        answer: 3
+    },
+    {
+        question: "Which programming language was created by Microsoft and is widely used for developing Windows applications and games?",
+        options:[ " C#", "Python" , "PHP" ,"Ruby"],
+        answer: 1
     },
 ];
